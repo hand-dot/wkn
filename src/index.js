f870a102c1b66933b7086b4596b5ea6882246126
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-09-29 04:00:10 +0900

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 export const wkn = (func, arg) => {
-    const funcStr = Function(`return${' '}${func}`);
-    const worker = new Worker(window.URL.createObjectURL(new Blob([
-        `onmessage = async (e) => {
-            postMessage(await ${funcStr}()(e.data));
-        }`,
-    ], { type: 'text/javascript' })));
-    worker.postMessage(arg);
-    return new Promise((resolve, reject) => {
-      worker.onerror = (e) => {
-        reject(e.message);
-        worker.terminate();
-      };
-      worker.onmessage = (e) => {
+  const worker = new Worker(window.URL.createObjectURL(new Blob([
+    `onmessage = (e) => {
+new Promise((resolve, reject) => {
+  try{
+    resolve(${Function(`return${' '}${func}`)}()(e.data));
+  }catch(e){
+    reject(e);
+  }
+}).then(res=>{
+  postMessage(res);
+},err=>{
+  postMessage(JSON.parse(JSON.stringify({wknError: err.message})));
+})
+}`,
+  ], { type: 'text/javascript' })));
+  worker.postMessage(arg);
+  return new Promise((resolve, reject) => {
+    worker.onerror = (e) => {
+      reject(e.message);
+      worker.terminate();
+    };
+    worker.onmessage = (e) => {
+      if (e.data.wknError) {
+        reject(e.data.wknError.toString());
+      } else {
         resolve(e.data);
-        worker.terminate();
-      };
-    });
+      }
+      worker.terminate();
+    };
+  });
 }
