06e62fe8facf78c0ca44de7abd87e1299f281bcb
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-09-29 23:04:29 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
-var wkn = exports.wkn = function wkn(func, arg) {
-    var worker = new Worker(window.URL.createObjectURL(new Blob(['onmessage = ' + func], {
-        type: 'text/javascript'
-    })));
-    worker.postMessage(arg);
-    return new Promise(function (resolve, reject) {
-        worker.onerror = function (e) {
-            reject(e.message);
-        };
-        worker.onmessage = function (e) {
-            resolve(e.data);
-        };
-    });
+var wkn = exports.wkn = function wkn(func) {
+  for (var _len = arguments.length, arg = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    arg[_key - 1] = arguments[_key];
+  }
+
+  var data = 'onmessage = (e) => {\n  new Promise((resolve, reject) => {\n    try{\n      resolve(' + Function('return' + ' ' + func) + '().apply(this, e.data));\n    }catch(e){\n      reject(e);\n    }\n  }).then(res =>{\n    postMessage(res);\n  },err =>{\n    postMessage(\'wknERROR: \' + err.message);\n  });\n}';
+  var blob = new Blob([data], { type: 'text/javascript' });
+  var url = window.URL.createObjectURL(blob);
+  var worker = new Worker(url);
+  worker.postMessage(arg);
+  return new Promise(function (resolve, reject) {
+    worker.onmessage = function (e) {
+      if (e.data.toString().startsWith('wknERROR: ')) {
+        reject(e.data.replace('wknERROR: ', ''));
+      } else {
+        resolve(e.data);
+      }
+      worker.terminate();
+    };
+  });
 };
