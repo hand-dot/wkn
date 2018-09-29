f05ce05c71c126aa185def299bb64be5d623dce2
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-09-29 23:58:34 +0900

    add testcase

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,11 +7,27 @@ describe('Test of wkn', () => {
       content: wkn.toString()
     })
   });
-  it('wkn get resolve when good function', async () => {
+  it('wkn get resolve when good function1', async () => {
+    await expect(page.evaluate(`(async () => wkn((arg) => arg + 100, 10))()`)).resolves.toBe(110);
+  });
+  it('wkn get resolve when good function2', async () => {
     await expect(page.evaluate(`(async () => wkn((arg) => arg + "!", "hoge"))()`)).resolves.toBe('hoge!');
   });
-  it('wkn get resolve when some args function', async () => {
+  it('wkn get resolve when some args function1', async () => {
+    await expect(page.evaluate(`(async () => wkn((arg1, arg2) => arg1 + arg2, 10, 100))()`)).resolves.toBe(110);
+  });
+  it('wkn get resolve when some args function2', async () => {
     await expect(page.evaluate(`(async () => wkn((arg1, arg2) => arg1 + "!" + arg2 + "!", "hoge", "foo"))()`)).resolves.toBe('hoge!foo!');
+  });
+  it('wkn get resolve with importScripts', async () => {
+    await expect(page.evaluate(`(async () => 
+      wkn(
+        (arg1) => {
+          importScripts('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.js');
+          const m = moment(new Date(2011, 2, 12, 5, 0, 0));
+          return m.hours()
+        }, "hoge")
+    )()`)).resolves.toBe(5);
   });
   it('wkn get rejects when bat function', async () => {
     await expect(page.evaluate(`(async () => wkn((arg) => arg.map( _ =>  _ + '!'), {}))()`)).rejects.toThrowError('arg.map is not a function');
